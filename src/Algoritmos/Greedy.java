839fae1fc8a74d994d78d7c2d81077c4acab84df
pisarraguido <pisarraguido@gmail.com>
2023-06-11 23:31:39 -0300

    subo greedy funcionando con grafo no dirigido, lo guardo para cambiarlo a un grafo dirigido, para saber si tiene mas sentido

--- src/Algoritmos/Greedy.java
+++ src/Algoritmos/Greedy.java
@@ -4,13 +4,14 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import Grafo.Arco;
 import Grafo.GrafoNoDirigido;
 import Timer.Timer;
 
 public class Greedy {
-	private int distanciaMaxima =  9999999;
+	private int distanciaMaxima =  Integer.MAX_VALUE;
 	private double timer;
 	
 	public void greedy(GrafoNoDirigido<Integer> grafo, int origen, int destino){
@@ -23,92 +24,82 @@ public class Greedy {
 		
 		while(vertices.hasNext()) {
 			int vertice = vertices.next();
-			distancia.put(vertice, distanciaMaxima);
-			padre.put(vertice, null);
+			distancia.put(vertice, distanciaMaxima );
+			padre.put(vertice, -1);
 		}
-		
 		distancia.replace(origen, 0);
 
-		vertices = grafo.obtenerVertices();
-		while(vertices.hasNext()) {
-			int vertice = vertices.next();
-			int u = obtenerAdyacenteMenorDistancia(grafo, vertice, resultado);
-			resultado.add(vertice);
+		while(resultado.size()<grafo.cantidadVertices()) {
+			int u = obtenerAdyacenteMenorDistancia(distancia, resultado);
+			resultado.add(origen);
 
-			Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(u);
-			while(adyacentes.hasNext()) {
-				int adyacente = adyacentes.next();
-
-				if(distancia.get(vertice) + distanciaEntre(grafo,u,adyacente)<distancia.get(adyacente)) {
-					distancia.replace(adyacente,distancia.get(vertice) + distanciaEntre(grafo,u,adyacente));
-					padre.replace(vertice, u);
-					System.out.println(padre.toString());
-				}
-
-
-			}	
-
+			if(u!=destino) {
+				Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(u);
+				while(adyacentes.hasNext()) {
+					int adyacente = adyacentes.next();
+					if(!resultado.contains(adyacente)) {
+						int dist = distancia.get(u) + distanciaEntre(grafo,u,adyacente);
+						if(dist < distancia.get(adyacente)) {
+							distancia.replace(adyacente,dist);
+							padre.replace(adyacente, u);
+						}
+					}					
+				}				
+			}
 		}
 		this.timer = timer.stop();
 		if(!resultado.isEmpty()) {
-			this.mostrarSolucion(padre, distancia);
+			this.mostrarSolucion(padre, distancia, destino);
 		}else {
-			this.mostrarSolucion(null,distancia);
+			this.mostrarSolucion(null,distancia, destino);
 		}
 		
 	}
 	
 	
-//	for(int i = 0 ; i<resultado.size(); i++) {
-//	int adyacente = resultado.get(i);
-//	System.out.println("adyacente "+ adyacente);
-//	if(distancia.get(u) + distanciaEntre(grafo,u,vertice)<distancia.get(vertice)) {
-//		System.out.println("Distancia adya "+distancia.get(adyacente) + " " + adyacente);
-//		System.out.println("Distancia vertice y u "+distanciaEntre(grafo,u,vertice));
-//		distancia.replace(adyacente,distancia.get(u) + distanciaEntre(grafo,u,vertice) );
-//		padre.replace(vertice, u);					
-//	}
-//	
-//}
-	
-	private int obtenerAdyacenteMenorDistancia(GrafoNoDirigido<Integer> grafo, int origen, List<Integer> resultadoParcial) {
-		Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(origen);
-		int verticeMenor=0;
-		float distancia = 9999999;
-		while(adyacentes.hasNext()) {
-			int adyacente = adyacentes.next();
-			Arco<Integer> arco = grafo.obtenerArco(origen, adyacente);
-			if(arco!=null && arco.getEtiqueta()<distancia && !resultadoParcial.contains(adyacente)) {
-				distancia = arco.getEtiqueta();
-				verticeMenor = adyacente;
+	private int obtenerAdyacenteMenorDistancia(HashMap<Integer,Integer> distancias, List<Integer> resultadoParcial) {
+		int verticeMenor = -1;
+		float distanciaMenor = this.distanciaMaxima;
+		for(Map.Entry<Integer, Integer> entry : distancias.entrySet()) {
+			int vertice = entry.getKey();
+			int dist = entry.getValue();
+			if(!resultadoParcial.contains(vertice)&& dist<distanciaMenor) {
+				distanciaMenor = dist;
+				verticeMenor = vertice;
 			}
-		}	
+		}
 		return verticeMenor;
 	}
 	
 	private Integer distanciaEntre(GrafoNoDirigido<Integer> grafo,int origen, int destino) {
-		Arco<Integer> arco = grafo.obtenerArco(origen, destino);
-		if(grafo.existeArco(origen, destino)) {
-			return arco.getEtiqueta();
+		Iterator<Integer> adyacentes =  grafo.obtenerAdyacentes(origen);
+		while(adyacentes.hasNext()) {
+			int adyacente = adyacentes.next();
+			if(grafo.existeArco(origen, destino)) {
+				Arco<Integer> arco = grafo.obtenerArco(origen, destino);
+				return arco.getEtiqueta();
+				
+			}
 		}
-		return distanciaMaxima;
+		return 0;
 	}
 	
-	private void mostrarSolucion(HashMap<Integer,Integer> padre, HashMap<Integer,Integer>distancia) {
+	private void mostrarSolucion(HashMap<Integer,Integer> padre, HashMap<Integer,Integer>distancia, int destino) {
 		int distanciaTotal = 0;
+		LinkedList<Integer> camino = new LinkedList<Integer>();
+		int vertice  = destino;
+		
 		if(padre!=null) {
-			for(Integer key:padre.keySet()) {
-				Integer value = padre.get(key);
-				System.out.print(value + " ");
-			}
-			for(Integer key: distancia.keySet() ) {
-				Integer value = distancia.get(key);
-				distanciaTotal = distanciaTotal + value;
+			while(vertice != -1) {
+				distanciaTotal = distanciaTotal + distancia.get(vertice);
+				camino.addFirst(vertice);
+				vertice = padre.get(vertice);
 			}
 		}else {
 			System.out.println("No se pudo determinar una solución");
 		}
 		System.out.println();
+		System.out.println(camino.toString());
 		System.out.println(distanciaTotal + " Kms");
 		System.out.println("Tiempo: "+this.timer);
 	}
