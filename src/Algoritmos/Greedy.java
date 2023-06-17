14de0883f81789b7065e261417fc615e47096283
pisarraguido <pisarraguido@gmail.com>
2023-06-16 21:43:23 -0300

    correccion greedy, creo que ahora esta bien

--- src/Algoritmos/Greedy.java
+++ src/Algoritmos/Greedy.java
@@ -1,29 +1,85 @@
 package Algoritmos;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import AlgoritmosOrdenamiento.QuickSort;
 import Grafo.Arco;
-import Grafo.GrafoDirigido;
 import Grafo.GrafoNoDirigido;
-import Timer.Timer;
 
 public class Greedy {
-    private int distanciaMaxima = Integer.MAX_VALUE;
-    private double timer;
+    private GrafoNoDirigido<Integer> grafo;
+    private int time = 0;
+    
+    public void greedy (GrafoNoDirigido<Integer> grafo){
+        HashMap<Integer,Integer> resultado = new HashMap<Integer,Integer>(); 
 
+        this.grafo = grafo;
+    	
+    	Iterator<Arco<Integer>> listaArcos = ordenarArcosPorMenorDistancia();
+    	    	
+    	while(listaArcos.hasNext()) {
+    		Arco<Integer> arco = listaArcos.next();
+    		this.time++;
+    		if(!resultado.containsKey(arco.getVerticeOrigen()) &&  !resultado.containsKey(arco.getVerticeDestino())) {   
+    				resultado.put(arco.getVerticeOrigen(),arco.getVerticeDestino());
+    		}
+    	}
+    	if(!resultado.isEmpty()) {
+    		mostrarSolucion(resultado);
+    	}else {
+    		System.out.println("No hay solucion");
+    	}   
+    }
     
+    private void mostrarSolucion(HashMap<Integer,Integer> solucion) {
+    	int distancia = 0 ;
+    	int i = 0;
+    	for (Map.Entry<Integer, Integer> entry : solucion.entrySet()) {
+          System.out.print("E"+entry.getKey()+ "-" +"E"+entry.getValue());
+          if (i < solucion.size() - 1) {
+              System.out.print(", ");
+          }
+          i++;
+          Arco<Integer> arco = this.grafo.obtenerArco(entry.getKey(), entry.getValue());
+          distancia = distancia + arco.getEtiqueta();
+    	}
+    	System.out.println();
+        System.out.println(distancia+" Kms");
+        System.out.println(time+" Iteraciones");
+
+
+    }
     
-    
-    
-    
-    
-    
-    
+    /*
+     * Complejidad computacional con notación big O 
+     * En el peor caso (los arcos estan desordenados) la complejidad es de O(n^2)
+     * En el mejor caso (los arcos estan ordenados) la complejidad computacional es de O(n) 
+     */
+    private Iterator<Arco<Integer>>  ordenarArcosPorMenorDistancia(){
+    	Iterator<Arco<Integer>> listadoAuxiliar = grafo.obtenerArcos();
+    	List<Arco<Integer>> listaArcos = new ArrayList<>();
+
+    	while (listadoAuxiliar.hasNext()) {
+    	    Arco<Integer> arco = listadoAuxiliar.next();
+    	    listaArcos.add(arco);
+    	}
+
+    	for (int i = 1; i < listaArcos.size(); i++) {
+    	    Arco<Integer> arcoActual = listaArcos.get(i);
+    	    int j = i - 1;
+    	    while (j >= 0 && listaArcos.get(j).getEtiqueta() > arcoActual.getEtiqueta()) {
+    	        listaArcos.set(j + 1, listaArcos.get(j));
+    	        j--;
+    	    }
+    	    listaArcos.set(j + 1, arcoActual);
+    	}
+
+    	Iterator<Arco<Integer>> iteratorOrdenado = listaArcos.iterator();
+    	return iteratorOrdenado;
+    }
     
     
 // TERCER SOLUCION (NO SIRVE)    
